b527c922121a130a134d23536bb00f5a3869d7aa
Jakub Klinicki <kubaklinicki@gmail.com>
2020-02-29 13:46:46 +0100

    Task-7. Add save and open file.

---
+++ src/main/java/Aplications/Main.java
@@ -0,0 +1,27 @@
+package Aplications;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    String resourcePath = "/fxml/Main.fxml";
+    private static Stage mainStage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        mainStage = primaryStage;
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource(resourcePath));
+        Parent root = loader.load();
+        Scene scene = new Scene(root, 600, 400);
+        primaryStage.setTitle("Enigma");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static Stage getMainStage() {
+        return mainStage;
+    }
+}
